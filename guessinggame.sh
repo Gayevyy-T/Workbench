a4f275e2b05d89b46e1d57cbc49982c886fee9c6
Gayevyy-T <tarasa1987@gmail.com>
2021-01-27 13:52:27 +0100

    guessinggame

---
+++ guessinggame.sh
@@ -0,0 +1,30 @@
+
+
+#read -ep "how many files are in the current directory: "
+#n="$REPLY"
+#echo $n
+###OR###
+echo "how many files are in the current directory: "
+read n
+#echo $n
+
+#f=$(ls -A | wc -l)	#in case hide files are needed
+f=$(ls | wc -l)
+#echo $f
+
+guessn() {
+	while [[ $n -ne $f ]]
+	do
+		if [[ $n -lt $f ]]
+		then
+			echo "the number is too small, try again: "
+		else
+			[[ $n -gt $f ]]
+			echo "the number is too big, try again: "
+		fi
+		read n
+	done
+echo "Greate, the number is correct"
+}
+
+guessn
